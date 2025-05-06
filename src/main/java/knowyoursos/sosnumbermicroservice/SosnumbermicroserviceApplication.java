1a7f670d66cfcef6f6e1756225a64fcdcbb20824
andresOchoaHernandez <ada.wong.gv@gmail.com>
2025-05-06 23:20:24 +0200

    Generated project with spring initializer

---
+++ src/main/java/knowyoursos/sosnumbermicroservice/SosnumbermicroserviceApplication.java
@@ -0,0 +1,13 @@
+package knowyoursos.sosnumbermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosnumbermicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosnumbermicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/knowyoursos/sosnumbermicroservice/SosnumbermicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package knowyoursos.sosnumbermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosnumbermicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
